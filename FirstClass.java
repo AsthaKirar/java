32d07db6ffc41a01f5dd708ff16cd25904830f56
AsthaKirar <asthap602@gmail.com>
2024-09-30 13:29:36 +0530

    commit

--- FirstClass.java
+++ FirstClass.java
@@ -1,6 +1,36 @@
-class FirstClass {
-    public static void main(String args[]){
-        System.out.println("Hello World");
+// class FirstClass {
+//     public static void main(String args[]){
+//         System.out.println("Hello World");
         
+//     }
+// }
+// class FirstClass {
+//     public static void main(String args[]){
+//         System.out.print("hello astha in java \nwelcome to apna college");
+//     }
+// }
+
+
+//Q. print the pattern
+
+// class FirstClass{
+//     public static void main(String args[]){
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+        
+
+//     }
+// }
+
+
+
+class FirstClass{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+       int sum = a+b;
+        System.out.println(sum);
     }
 }
