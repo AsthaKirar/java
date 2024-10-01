989aed9db421173ec6acb2b9febc10e9b63811b0
AsthaKirar <asthap602@gmail.com>
2024-10-01 23:31:25 +0530

    commit

--- FirstClass.java
+++ FirstClass.java
@@ -13,16 +13,16 @@
 
 //Q. print the pattern
 
-class FirstClass{
-    public static void main(String args[]){
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+// class FirstClass{
+//     public static void main(String args[]){
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
         
 
-    }
-}
+//     }
+// }
 
 
 
@@ -34,3 +34,100 @@ class FirstClass{
 //         System.out.println(sum);
 //     }
 // }
+
+
+//input lena in java
+
+// import java.util.*;
+
+//  class FirstClass {
+
+    
+//      public static void main(String[] args){
+        
+//         Scanner sc = new Scanner(System.in);
+//         String name = sc.nextLine();
+//         System.out.println(name);
+
+
+//     }
+
+// }
+
+
+// Q take a variable a and b and print their sum.
+// import java.util.*;
+//  class FirstClass {
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a+b;
+//         System.out.println(sum);
+
+
+
+
+        
+//     }
+// }
+
+
+
+//conditional statement
+
+// import java.util.*;
+// public class FirstClass {
+
+//     public static void main(String[] args){
+//         Scanner sc =  new Scanner(System.in);
+//         int age = sc.nextInt();
+//         if(age>18){
+//             System.out.println("adult");
+//         }
+//         else{
+//             System.out.println("Not adult");
+//         }
+//     }
+    
+// }
+
+// import java.util.*;
+// public class FirstClass {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int x = sc.nextInt();
+//         if(x%2==0){
+//             System.out.println("even");
+//         }
+//         else{
+//             System.out.println("odd");
+//         }
+//     }
+
+    
+// }
+
+import java.util.*;
+
+public class FirstClass {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a==b){
+            System.out.println("equal");
+        }
+        else if(a>b){
+            System.out.println("a is greater than b");
+        }
+        else{
+            System.out.println("a is lesser");
+        }
+    }
+
+    
+}
+
