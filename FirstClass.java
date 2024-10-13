237ba6f119054911fe4fdf710a336ecf1b4bf5c9
AsthaKirar <asthap602@gmail.com>
2024-10-13 14:31:34 +0530

    commit

--- FirstClass.java
+++ FirstClass.java
@@ -109,25 +109,35 @@
     
 // }
 
-import java.util.*;
+// import java.util.*;
 
+// public class FirstClass {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+
+//         if(a==b){
+//             System.out.println("equal");
+//         }
+//         else if(a>b){
+//             System.out.println("a is greater than b");
+//         }
+//         else{
+//             System.out.println("a is lesser");
+//         }
+//     }
+
+    
+// }
+
+import java.util.*;
 public class FirstClass {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-
-        if(a==b){
-            System.out.println("equal");
-        }
-        else if(a>b){
-            System.out.println("a is greater than b");
-        }
-        else{
-            System.out.println("a is lesser");
-        }
+        System.out.println(a+b);
     }
-
-    
 }
-
